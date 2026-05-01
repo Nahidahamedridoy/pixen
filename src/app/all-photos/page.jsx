e58913a043e12d3aa732a4352ebfd0fc9d638abc
Nahidahamedridoy <nahidahamedridoy@gmail.com>
2026-05-01 23:09:41 +0600

    added all photos pages

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,19 @@
+import PhotoCart from '@/components/PhotoCart';
+import React from 'react';
+
+const AllPhotosPage = async () => {
+    const res = await fetch('https://pixen-blond.vercel.app/data.json');
+    const photos = await res.json()
+    console.log(photos);
+    return (
+        <div>
+            <h1 className='text-2xl font-bold m-4'>All Photos</h1>
+
+            <div className="grid grid-cols-4 gap-5">
+            {photos.map (photo =><PhotoCart key={photo.id} photo={photo}/>)}
+            </div>
+        </div>
+    );
+};
+
+export default AllPhotosPage;
